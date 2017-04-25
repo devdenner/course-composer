5ffc8c39a486942e9afbe26dc5fe8f12b68ad054
Denner Fernandes <denners777@hotmail.com>
2017-04-24 22:57:14 -0300

    implements_phpunit
    Signed-off-by: Denner Fernandes <denners777@hotmail.com>

---
+++ tests/devDenner/SON/HelloPackageTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace devDenner\SON;
+
+class HelloPackageTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGetHello()
+    {
+        $hello = new HelloPackage();
+        $string = $hello->getHello();
+        $this->assertNotNull($string);
+        $this->assertInternalType('string', $string);
+        $this->assertNotEquals(0, strlen($string));
+
+    }
+
+}
